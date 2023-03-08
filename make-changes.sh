37cfa89bae4cdef823bc5aa6ddd106fab8e42b35
gitbite debug <bitegh20230307@gmail.com>
2023-03-08 17:18:07 -0600

    1678317484 commit message goes here (debug)

--- make-changes.sh
+++ make-changes.sh
@@ -6,7 +6,8 @@ git pull
 #git checkout -b $NOW
 echo "NOW IS: $NOW, TODAY IS: $DATE"
 echo $DATE>>log.txt
-git add .
+git add -A
 git commit -m "$NOW commit message goes here (debug)"
-git push --set-upstream origin $NOW
+#git push --set-upstream origin $NOW
+git push
 #git checkout main
