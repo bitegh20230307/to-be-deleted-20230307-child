d67e0a996f1448630113e48acdc557c27a7dc430
gitbite debug <bitegh20230307@gmail.com>
2023-03-08 17:17:02 -0600

    1678317418 commit message goes here (debug)

--- make-changes.sh
+++ make-changes.sh
@@ -3,10 +3,10 @@
 NOW=`date +%s`
 DATE=`date`
 git pull
-git checkout -b $NOW
+#git checkout -b $NOW
 echo "NOW IS: $NOW, TODAY IS: $DATE"
 echo $DATE>>log.txt
 git add .
 git commit -m "$NOW commit message goes here (debug)"
 git push --set-upstream origin $NOW
-git checkout main
+#git checkout main
